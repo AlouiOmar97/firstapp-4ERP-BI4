3fbbe17d955c0f5cc5caa300ccd9c0d86038257d
AlouiOmar97 <omar.aloui@esprit.tn>
2026-02-10 15:56:48 +0100

    Manipulation des composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ListSuggestionComponent
   ],
   imports: [
     BrowserModule,
